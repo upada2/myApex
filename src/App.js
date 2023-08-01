74f5e968b8666b5c6029fa44ab83a5dec07d4305
upada2 <upada2@uis.edu>
2023-07-31 22:23:17 -0400

    added new one

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>ApeX</p>
+
+        <p></p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ApeX R
         </a>
       </header>
     </div>
